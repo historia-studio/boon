3465122191151c647681e3d49e6850844631ba37
Historia Studio Inc <business@historia.studio>
2026-03-18 15:02:07 -0600

    Add util bat files

---
+++ server.bat
@@ -0,0 +1,3 @@
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" amd64
+set PATH=C:\ProgramData\chocolatey\lib\elixir\tools\bin;%PATH%
+mix.bat phx.server
